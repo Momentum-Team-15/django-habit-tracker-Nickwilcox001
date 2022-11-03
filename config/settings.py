0f6e8d9b2a4041f004df101356bd8d622169fac2
Nick Wilcox <Nickwilcox001@gmail.com>
2022-11-03 01:57:09 -0400

    Last troubleshoot for the night

--- config/settings.py
+++ config/settings.py
@@ -128,10 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR , "static")
-]
-STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT  = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR , "static")
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
 
 if not DEBUG: 
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
